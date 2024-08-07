24bfa940e88250c149f589cfd55de6352c91cea8
diego.oberto <diegooberto9@gmail.com>
2024-08-07 14:52:49 -0400

    main.js in develpment

---
+++ main.js
@@ -0,0 +1,9 @@
+const Buttons = document.querySelectorAll('button');
+const display = document.getElementById('display');
+
+// variables
+FirstValue=null;
+SecondValue=null;
+operand=null
+Result=0
+console.log(Buttons);
