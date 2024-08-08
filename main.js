fd96d9e4ba3eb7582f749e0ffe30e006e1593786
diego.oberto <diegooberto9@gmail.com>
2024-08-08 13:36:13 -0400

    adding functionality

--- main.js
+++ main.js
@@ -4,11 +4,26 @@ const display = document.getElementById('display');
 // variables
 FirstValue=null;
 SecondValue=null;
-operand=null
-Result=0
+operand=null;
+Result=0;
+
 
 // functions
+Buttons.forEach((element) => {
+   
+    element.addEventListener("click",() => {
+            
+        if (element.id === "number"){
+            if(display.innerText == 0){
+                display.innerText = element.innerText
+            }
+            else{
+               display.innerText += element.innerText
+            console.log(`este es numero ${element.innerText}`)  
+            }
+            }           
+            }) 
+            });
 
-function asignValue(element){
-    element.value
-}
+
+
