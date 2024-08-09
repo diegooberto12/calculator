e9f9e3c995449eac7406ed93afa91cf62b57f23f
diego.oberto <diegooberto9@gmail.com>
2024-08-09 11:23:47 -0400

    adde function of results

--- main.js
+++ main.js
@@ -24,7 +24,8 @@ Buttons.forEach((element) => {
             updateDisplay(); 
             }
             }
-        else if (element.id === "plus"){
+        else if (element.id === "operator"){
+            
             
             
         }           
@@ -33,16 +34,76 @@ Buttons.forEach((element) => {
 
 function updateDisplay(){
 if (display.innerText.length > 7){
-    display.innerText=display.innerText.slice(0,7)
+    display.innerText=display.innerText.slice(0,7);
+}
+else if (display.innerText.includes(".")){
+    display.innerText=display.innerText.slice(0,8);
+}
+else if(display.innerHTML.startsWith("-")){
+    display.innerText=display.innerText.slice(0,8);
+}
+else if (display.innerText.includes(".") && display.innerHTML.startsWith("-")) {
+    display.innerText=display.innerText.slice(0,9);
 }
 };
 
-function checkSteps(){
-    if (FirstValue  != null && SecondValue == null){
-        return true
+
+function operate(element){
+    if (FirstValue  == null && SecondValue == null){
+            FirstValue=Number(display.innerText);
+            if (element.id == "operator"){
+                GiveValueToOperand(element);
+                display.innerText="";
+            }
+    else if(FirstValue != null && SecondValue == null && operand !=null){
+
     }
-    else {return false};
+        
+    }
+    // else if (display.textContent == 0 || display.textContent == "0"){
+    //     FirstValue = 0
+    // }
+    // else {return false};
 }
 
+function GiveValueToOperand(element){
+    switch (element){
+        case element.innerText == "+":
+            return operand =  "+";
+            break;
+        case element.innerText == "-":
+            return operand = "-";
+            break;
+        case element.innerText == "*":
+            return  operand = "*";
+            break;
+        case element.innerText == "/":
+            return  operand = "/";
+            break;
+    }
+};
+ function Showresult(){
+    
+ }
 
+function DoOperation(A , B , C){
+ switch (B){
+    case "+" :
+        return A + C;
+        break;
+    case "-":
+        return A - C;
+        break;
+    case "*":
+        return A * C;
+    case "/":
+        if (C == 0){
+            console.log("don't do that");     
+        }
+        else{
+            return A / C;
+        }
+        break;
+ }
 
+}
